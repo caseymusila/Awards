4253b37e01875cb4b28ea261f5f99447b223dc40
caseymusila <musilacasey@gmail.com>
2021-09-23 09:56:48 +0300

    we create class for our update profile form and model

--- awwarding/forms.py
+++ awwarding/forms.py
@@ -4,12 +4,12 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class CreateProfileForm(ModelForm)
+class CreateProfileForm(ModelForm):
   class Meta:
     model=Profile
     exclude =['user']
 
-class UpdateProfileForm(ModelForm):
+class UpdateProfileForm(ModelForm)
   class Meta:
     model=Profile
     exclude=['user']
