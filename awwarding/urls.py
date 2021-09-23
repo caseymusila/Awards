d2da0c9c7a863d9341e10a48d290ce36ab416614
caseymusila <musilacasey@gmail.com>
2021-09-23 09:52:29 +0300

    we create a url path to displaying a single project

--- awwarding/urls.py
+++ awwarding/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     re_path('profile/(?P<profile_id>\d+)',views.profile,name = 'profile'),
     path('create_project/',views.create_project,name = 'create_project'),
     re_path('edit_profile/<profile_id>',views.edit_profile,name='edit_profile'),
-    path('email/', views.email, name='email')
-    path('disp_project/<project_id>',views.disp_project, name='disp_project'),
+    path('email/', views.email, name='email'),
+    path('disp_project/<project_id>',views.disp_project, name='disp_project')
     
 ]
 
