472a96aaaf42cd4fc94064258a4bf0077f2c26cd
caseymusila <musilacasey@gmail.com>
2021-09-23 09:53:29 +0300

    we import from urls path include,repath

--- awwarding/urls.py
+++ awwarding/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('create_project/',views.create_project,name = 'create_project'),
     re_path('edit_profile/<profile_id>',views.edit_profile,name='edit_profile'),
     path('email/', views.email, name='email'),
-    path('disp_project/<project_id>',views.disp_project, name='disp_project')
+    path('disp_project/<project_id>',views.disp_project, name='disp_project'),
     
 ]
 
